f7b0d8df0323791f77616db32f37a9031368e7eb
Antonio Gámez Díaz <antgamdia@gmail.com>
2016-01-11 20:24:42 +0100

    New changes and models

--- demobuilder.scenario/src-gen/demobuilder.sh
+++ demobuilder.scenario/src-gen/demobuilder.sh
@@ -7,12 +7,12 @@ PROJECT_NAME="Amazon"
 curl -k -basic -u $USER:$PASS https://localhost:8181/IDEAS/file/createProject?projectUri=$WORKSPACE_NAME/$PROJECT_NAME
 			
 				
-FILE_NAME="planBasic.at"
+FILE_NAME="basicPlan.at"
 FILE_URI="$WORKSPACE_NAME/$PROJECT_NAME/$FILE_NAME"
 curl -k -basic -u $USER:$PASS -H "Accept: application/json" -H "Content-Type:application/json" https://localhost:8181/IDEAS/file/createFile?fileUri=$FILE_URI
 curl -k -u $USER:$PASS -X POST -d "fileUri=$FILE_URI" -d "fileContent=$(cat $FILE_NAME)" https://localhost:8181/IDEAS/file/setFileContent
 				
-FILE_NAME="planMedium.at"
+FILE_NAME="mediumPlan.at"
 FILE_URI="$WORKSPACE_NAME/$PROJECT_NAME/$FILE_NAME"
 curl -k -basic -u $USER:$PASS -H "Accept: application/json" -H "Content-Type:application/json" https://localhost:8181/IDEAS/file/createFile?fileUri=$FILE_URI
 curl -k -u $USER:$PASS -X POST -d "fileUri=$FILE_URI" -d "fileContent=$(cat $FILE_NAME)" https://localhost:8181/IDEAS/file/setFileContent
