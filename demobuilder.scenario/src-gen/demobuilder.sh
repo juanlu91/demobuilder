3c310af5f17c0ea5b318b31b84ddc089bc7fba71
Felipe Vieira da Cunha Serafim <feserafim@gmail.com>
2016-01-16 12:29:15 +0100

    Giving format to the bash file

--- demobuilder.scenario/src-gen/demobuilder.sh
+++ demobuilder.scenario/src-gen/demobuilder.sh
@@ -2,18 +2,38 @@
 USER="SMA"
 PASS="SMA"
 WORKSPACE_NAME="Workspace"
+echo
+echo "Creating workspace... "
 curl -k -basic -u $USER:$PASS https://antgamdiapc.lsi.us.es:9191/IDEAS/file/createWorkspace?workspaceName=$WORKSPACE_NAME
+echo
+echo
+echo "Creating $PROJECT_NAME project... "
 PROJECT_NAME="Amazon"
 curl -k -basic -u $USER:$PASS https://antgamdiapc.lsi.us.es:9191/IDEAS/file/createProject?projectUri=$WORKSPACE_NAME/$PROJECT_NAME
 			
 				
 FILE_NAME="basicPlan.at"
 FILE_URI="$WORKSPACE_NAME/$PROJECT_NAME/$FILE_NAME"
+echo
+echo
+echo "Creating file $FILE_NAME..."
 curl -k -basic -u $USER:$PASS -H "Accept: application/json" -H "Content-Type:application/json" https://antgamdiapc.lsi.us.es:9191/IDEAS/file/createFile?fileUri=$FILE_URI
+echo
+echo
+echo "Loading content to $FILE_URI..."
 curl -k -u $USER:$PASS -X POST -d "fileUri=$FILE_URI" -d "fileContent=$(cat $FILE_NAME)" https://antgamdiapc.lsi.us.es:9191/IDEAS/file/setFileContent
 
 FILE_NAME="basicPlan.opl"
 FILE_URI="$WORKSPACE_NAME/$PROJECT_NAME/$FILE_NAME"
+echo
+echo
+echo "Creating file $FILE_NAME..."
 curl -k -basic -u $USER:$PASS -H "Accept: application/json" -H "Content-Type:application/json" https://antgamdiapc.lsi.us.es:9191/IDEAS/file/createFile?fileUri=$FILE_URI
+echo
+echo
+echo "Loading content to $FILE_URI..."
 curl -k -u $USER:$PASS -X POST -d "fileUri=$FILE_URI" -d "fileContent=$(cat $FILE_NAME)" https://antgamdiapc.lsi.us.es:9191/IDEAS/file/setFileContent
 				
+
+echo
+echo
