3dba5f8da6f5f7008828fe265e88543ee618f35e
Felipe Vieira da Cunha Serafim <feserafim@gmail.com>
2016-01-16 12:14:00 +0100

    New demobuilder.sh working with SMA user

--- demobuilder.scenario/src-gen/demobuilder.sh
+++ demobuilder.scenario/src-gen/demobuilder.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-USER="DemoMaster"
-PASS="DemoMaster"
+USER="SMA"
+PASS="SMA"
 WORKSPACE_NAME="Workspace"
-curl -k -basic -u $USER:$PASS https://localhost:8181/IDEAS/file/createWorkspace?workspaceName=$WORKSPACE_NAME
+curl -k -basic -u $USER:$PASS https://antgamdiapc.lsi.us.es:9191/IDEAS/file/createWorkspace?workspaceName=$WORKSPACE_NAME
 PROJECT_NAME="Amazon"
-curl -k -basic -u $USER:$PASS https://localhost:8181/IDEAS/file/createProject?projectUri=$WORKSPACE_NAME/$PROJECT_NAME
+curl -k -basic -u $USER:$PASS https://antgamdiapc.lsi.us.es:9191/IDEAS/file/createProject?projectUri=$WORKSPACE_NAME/$PROJECT_NAME
 			
 				
 FILE_NAME="basicPlan.at"
 FILE_URI="$WORKSPACE_NAME/$PROJECT_NAME/$FILE_NAME"
-curl -k -basic -u $USER:$PASS -H "Accept: application/json" -H "Content-Type:application/json" https://localhost:8181/IDEAS/file/createFile?fileUri=$FILE_URI
-curl -k -u $USER:$PASS -X POST -d "fileUri=$FILE_URI" -d "fileContent=$(cat $FILE_NAME)" https://localhost:8181/IDEAS/file/setFileContent
-				
-FILE_NAME="mediumPlan.at"
+curl -k -basic -u $USER:$PASS -H "Accept: application/json" -H "Content-Type:application/json" https://antgamdiapc.lsi.us.es:9191/IDEAS/file/createFile?fileUri=$FILE_URI
+curl -k -u $USER:$PASS -X POST -d "fileUri=$FILE_URI" -d "fileContent=$(cat $FILE_NAME)" https://antgamdiapc.lsi.us.es:9191/IDEAS/file/setFileContent
+
+FILE_NAME="basicPlan.opl"
 FILE_URI="$WORKSPACE_NAME/$PROJECT_NAME/$FILE_NAME"
-curl -k -basic -u $USER:$PASS -H "Accept: application/json" -H "Content-Type:application/json" https://localhost:8181/IDEAS/file/createFile?fileUri=$FILE_URI
-curl -k -u $USER:$PASS -X POST -d "fileUri=$FILE_URI" -d "fileContent=$(cat $FILE_NAME)" https://localhost:8181/IDEAS/file/setFileContent
+curl -k -basic -u $USER:$PASS -H "Accept: application/json" -H "Content-Type:application/json" https://antgamdiapc.lsi.us.es:9191/IDEAS/file/createFile?fileUri=$FILE_URI
+curl -k -u $USER:$PASS -X POST -d "fileUri=$FILE_URI" -d "fileContent=$(cat $FILE_NAME)" https://antgamdiapc.lsi.us.es:9191/IDEAS/file/setFileContent
 				
